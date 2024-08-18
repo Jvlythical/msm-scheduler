1b7ed301d474e2e01c876f143c98091bdb72591b
jvlythical <lvlichael8@gmail.com>
2024-08-18 07:27:20 -0700

    Fix odd interests column naming

--- msm_scheduler/core/transformers/csv_to_player_interests.py
+++ msm_scheduler/core/transformers/csv_to_player_interests.py
@@ -9,12 +9,12 @@ class CSVToPlayerInterestsTransformer():
     experiences = []
     for row in self.rows:
       experience = {
-        HARD_DAMIEN: self.is_interested(row['Hard Damien']),
-        LOTUS: self.is_interested(row['Lotus']),
-        LUCID: self.is_interested(row['Lucid']),
+        HARD_DAMIEN: self.is_interested(row.get('Hard Damien') or row.get('Hard Damien_interest')),
+        LOTUS: self.is_interested(row.get('Lotus') or row.get('Lotus_interest')),
+        LUCID: self.is_interested(row.get('Lucid') or row.get('Lucid_interest')),
         'name': row['Name'],
-        NORMAL_DAMIEN: self.is_interested(row['Normal Damien']),
-        WILL: self.is_interested(row['Will'])
+        NORMAL_DAMIEN: self.is_interested(row.get('Normal Damien') or row.get('Normal Damien_interest')),
+        WILL: self.is_interested(row.get('Will') or row.get('Will_interest'))
       }
 
       experiences.append(experience)
